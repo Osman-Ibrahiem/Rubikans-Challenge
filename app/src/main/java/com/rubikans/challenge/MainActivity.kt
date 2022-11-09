80baabebf025b845a632bea3e3366ee100859d02
Osman Ibrahiem <osman.ibrahiem187@gmail.com>
2022-11-09 23:56:02 +0200

    Setup dependencies for modules

--- app/src/main/java/com/rubikans/challenge/MainActivity.kt
+++ app/src/main/java/com/rubikans/challenge/MainActivity.kt
@@ -2,7 +2,9 @@ package com.rubikans.challenge
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
