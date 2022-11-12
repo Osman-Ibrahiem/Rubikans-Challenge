432e283ded8ad38a0dd5c15b81ecc3f91ca2c732
Osman Ibrahiem <osman.ibrahiem187@gmail.com>
2022-11-12 14:19:46 +0200

    Added round corner on settings adapter

--- app/src/main/java/com/rubikans/challenge/ui/settings/SettingsAdapter.kt
+++ app/src/main/java/com/rubikans/challenge/ui/settings/SettingsAdapter.kt
@@ -6,6 +6,8 @@ import androidx.core.view.isVisible
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.card.MaterialCardView
+import com.google.android.material.shape.CornerFamily.ROUNDED
 import com.rubikans.challenge.databinding.ItemSettingListBinding
 import com.rubikans.challenge.domain.model.SettingType
 import com.rubikans.challenge.domain.model.Settings
@@ -13,6 +15,12 @@ import com.rubikans.challenge.ui.base.BaseAdapter
 import javax.inject.Inject
 
 class SettingsAdapter @Inject constructor() : BaseAdapter<Settings>() {
+
+    companion object {
+        private const val TOP = 0
+        private const val MIDDLE = 1
+        private const val BOTTOM = 2
+    }
 
     private val diffCallback = object : DiffUtil.ItemCallback<Settings>() {
         override fun areItemsTheSame(oldItem: Settings, newItem: Settings): Boolean {
@@ -29,10 +37,21 @@ class SettingsAdapter @Inject constructor() : BaseAdapter<Settings>() {
     override fun getViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val binding =
             ItemSettingListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return SettingsViewHolder(binding)
+        return SettingsViewHolder(binding, viewType)
     }
 
-    inner class SettingsViewHolder(private val binding: ItemSettingListBinding) :
+    override fun getItemViewType(position: Int): Int {
+        return when (position) {
+            0 -> TOP
+            (list.size - 1) -> BOTTOM
+            else -> MIDDLE
+        }
+    }
+
+    inner class SettingsViewHolder(
+        private val binding: ItemSettingListBinding,
+        private val viewType: Int,
+    ) :
         RecyclerView.ViewHolder(binding.root), Binder<Settings> {
         override fun bind(item: Settings, position: Int) {
             binding.apply {
@@ -66,11 +85,30 @@ class SettingsAdapter @Inject constructor() : BaseAdapter<Settings>() {
                         textViewValue.isVisible = false
                     }
                 }
+                when (viewType) {
+                    TOP -> setRadius(binding.cardView, true)
+                    BOTTOM -> setRadius(binding.cardView, false)
+                }
             }
         }
 
         private fun setClickListener(settings: Settings) {
             onItemClickListener(settings)
+        }
+
+        private fun setRadius(cardView: MaterialCardView, isTop: Boolean) {
+            val radius: Float =
+                cardView.context.resources.getDimension(com.intuit.sdp.R.dimen._10sdp)
+            val shareAppendable = cardView.shapeAppearanceModel.toBuilder().apply {
+                if (isTop) {
+                    setTopLeftCorner(ROUNDED, radius)
+                    setTopRightCorner(ROUNDED, radius)
+                } else {
+                    setBottomRightCorner(ROUNDED, radius)
+                    setBottomLeftCorner(ROUNDED, radius)
+                }
+            }.build()
+            cardView.shapeAppearanceModel = shareAppendable
         }
     }
 }
